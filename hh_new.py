61f2cb8af9df8a8d79bb0039fa043e8e10de43b3
coderosemarisha <marinabox74@gmail.com>
2019-11-11 13:25:52 +0300

    Complete version of parser

--- hh_new.py
+++ hh_new.py
@@ -1,16 +1,15 @@
-# делаю парсер всех вакансий по питону на hh по всей России(в данный момент для удобства только 1 город).Только с указанной зарплатой
+# parser of all vacancies for python on hh.ru throughout Russia (only with the specified salary), with chart display.
 import requests
-import csv
 from bs4 import BeautifulSoup as bs
-import pprint
 import numpy
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# обязательно нужно указать:
 headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36 OPR/64.0.3417.54'}
 
-base_url = 'https://hh.ru/search/vacancy?search_period=3&clusters=true&area=113&text=python&enable_snippets=true&page=0'
+base_url = 'https://hh.ru/search/vacancy?search_period=7&clusters=true&area=113&text=python&enable_snippets=true&page=0'
 
 
 def hh_parse(base_url, headers):
@@ -26,7 +25,7 @@ def hh_parse(base_url, headers):
             pagination = soup.find_all('a', attrs={'data-qa': 'pager-page'})
             count = int(pagination[-1].text)
             for i in range(count):
-                url = f'https://hh.ru/search/vacancy?search_period=3&clusters=true&area=113&text=python&enable_snippets=true&page={i}'
+                url = f'https://hh.ru/search/vacancy?search_period=7&clusters=true&area=113&text=python&enable_snippets=true&page={i}'
                 if url not in urls:
                     urls.append(url)
         except:
@@ -38,8 +37,6 @@ def hh_parse(base_url, headers):
         divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
         for div in divs:
             try:
-                title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
-                href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
                 salary = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'}).text.split('-')
                 city = div.find('span', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
                 new_salary = []
@@ -47,50 +44,78 @@ def hh_parse(base_url, headers):
                 salary2 = []
 
                 for item in salary:
-                    # убираем юникод
+                    # remove unicode
                     item = item.replace(u'\xa0', u' ')
                     new_salary.append(item)
                 for item in new_salary:
 
                     if 'руб'in item:
-                        # выбираем цифры из строки
+                        # select numbers from a string
                         salary2.append(int(''.join(filter(str.isdigit, item))))
-                        # считаем среднее арифметическое
+                        # calculation of arithmetic mean
                         salary_final = int(numpy.mean(salary2))
 
                     elif 'USD' in item:
                         salary2.append(int(''.join(filter(str.isdigit, item))))
                         new_item = numpy.mean(salary2)
-                        # ребеводим баксы в рубли
+                        # converting bucks into rubles
                         salary_final = int(new_item * 63.72)
 
                     elif item.find('EUR'):
                         salary2.append(int(''.join(filter(str.isdigit, item))))
                         new_item = numpy.mean(salary2)
-                        # переводим евро в рубли
+                        # converting euros to rubles
                         salary_final = int(new_item * 71.13)
 
                 jobs.append({
-                    'title': title,
-                    'href': href,
                     'salary': salary_final,
-                    'city': city,
+                    'city': city.split(',')[0],
                 })
+
             except:
                 pass
-            pprint.pprint(jobs)
+
     else:
         print('DONE. Status code =  ' + str(request.status_code))
     return jobs
 
 
-def files_writer(jobs):
-    with open('parsed_jobs.csv', 'w')as file:
-        a_pen = csv.writer(file)
-        a_pen.writerow(('Город', 'Вакансия', 'Зарплата', 'Ссылка'))
-        for job in jobs:
-            a_pen.writerow((job['city'], job['title'], job['salary'], job['href']))
-
-
 jobs = hh_parse(base_url, headers)
-files_writer(jobs)
+
+# Processing the data received in parsing. Grouping cities and calculate the average salary in a city
+data = pd.DataFrame(jobs)
+group_data = data.groupby('city')
+average = group_data.mean()
+average.to_csv('group_table.csv')
+
+# Reading the input file. Processing data from the file to display a graph of average salaries by city
+data = pd.read_csv('group_table.csv')
+
+city_list = []
+salary_list = []
+for city in data['city']:
+    city_list.append(city)
+
+for salary in data['salary']:
+    salary_list.append(int(salary))
+
+fig = plt.gcf()
+fig.canvas.set_window_title('Median salaries for Python developers in Russia')
+fig.set_size_inches(18.5, 10.5)
+plt.subplots_adjust(left=0.1, right=0.9, bottom=0.4, top=0.88)
+
+plt.bar(city_list, salary_list, color='dodgerblue', alpha=0.7)
+
+plt.xlabel('Median Salary', fontsize=16)
+plt.xticks(rotation=90)
+
+plt.title('Median salaries for Python developers in Russia', fontsize=16)
+plt.grid(axis='y')
+
+# Saving the figure on disk. 'dpi' and 'quality' can be adjusted according to the required image quality.
+plt.savefig('Median_salary.png', dpi=400, quality=100)
+
+# Displays the plot.
+plt.show()
+
+
